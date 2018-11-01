4f56f89005c0da4bb16f4748f454b9088f117e18
saket9000 <saket9000@gmail.com>
2018-11-02 00:33:03 +0530

    Excluding git ignore files

--- placement/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlacementConfig(AppConfig):
-    name = 'placement'
--- placement/helpers/drf_serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from talk.models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ('id', 'author', 'text', 'created', 'updated')
